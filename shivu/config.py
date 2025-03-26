55c111932852515d67b5670d7f25a94afe043fa0
Harsh04756 <alinwalker252@gmail.com>
2025-03-26 11:40:25 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6675050163"
-    sudo_users = "6675050163", "6087651372","7640076990","1420481421","6645960688"
-    GROUP_ID = -1002499806698
-    TOKEN = "8175826981:AAGrz7p7NiE6t1vDIKDBTdbhHrcjB7MGDAo"
-    mongo_url = "mongodb+srv://sunnymuduli584:<db_password>@sunny.s9g6h.mongodb.net/"
-    PHOTO_URL = ["https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg", "https://graph.org/file/0aa659508c1add9ae4c86-2b335aa5262b7b64d2.jpg"]
-    SUPPORT_CHAT = "Anime_Circle_Club"
-    UPDATE_CHAT = "Waifu_Chan_Bot_updates"
-    BOT_USERNAME = "@Waifu_Chan_Robot"
+    OWNER_ID = "7885908019"
+    sudo_users = "7885908019", "7640076990", "7756901810" 
+    GROUP_ID =-1002640379822
+    TOKEN = "7583740884:AAEsWTCQNJiOjNQdXw4MBFU9-rL5l82mLQA" 
+    mongo_url = "mongodb+srv://naruhina:harshitkichut@cluster0.03pl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+    PHOTO_URL = "https://files.catbox.moe/0nb1p7.jpg" 
+    SUPPORT_CHAT = "blade_x_support"
+    UPDATE_CHAT = "blade_x_community"
+    BOT_USERNAME = "Devine_wifu_bot"
     CHARA_CHANNEL_ID = "-1002640379822"
-    api_id = 28480539
-    api_hash = "6320d9f1bc1f0b72ad66ebdb9d6bfc2c"
+    api_id = 26626068
+    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
     
 class Production(Config):
